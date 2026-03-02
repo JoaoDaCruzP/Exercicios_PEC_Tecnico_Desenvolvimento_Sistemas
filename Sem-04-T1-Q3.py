2cb1cf94fd74ee93c1d73f620427d2638a486ddd
Joao da Cruz Desenvolvedor <99129744+JoaoDaCruzP@users.noreply.github.com>
2026-03-02 17:12:57 -0300

    Add files via upload

---
+++ Sem-04-T1-Q3.py
@@ -0,0 +1,5 @@
+'''Escreva um programa que leia um número inteiro “x” e escreva o valor desse número elevado ao cubo.'''
+print('Deseja saber o valor de um numero ao Cubo? ')
+x = int(input('Digite um valor inteiro: '))
+
+print(f'O cubo do valor digitado é: {x**3}')
