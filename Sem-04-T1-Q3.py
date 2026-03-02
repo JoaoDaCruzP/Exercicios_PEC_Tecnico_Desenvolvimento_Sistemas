9a5fb809f578f38230d1ee348e1da2101a973295
Joao da Cruz Desenvolvedor <99129744+JoaoDaCruzP@users.noreply.github.com>
2026-03-02 17:14:19 -0300

    Delete Sem-04-T1-Q3.py

--- Sem-04-T1-Q3.py
+++
@@ -1,5 +0,0 @@
-'''Escreva um programa que leia um número inteiro “x” e escreva o valor desse número elevado ao cubo.'''
-print('Deseja saber o valor de um numero ao Cubo? ')
-x = int(input('Digite um valor inteiro: '))
-
-print(f'O cubo do valor digitado é: {x**3}')
