4fe274246d0d01b1e8f856869b0df2d437f9b134
Joao da Cruz Desenvolvedor <99129744+JoaoDaCruzP@users.noreply.github.com>
2026-03-02 17:13:30 -0300

    Delete Sem-04-T1-Q1.py

--- Sem-04-T1-Q1.py
+++
@@ -1,8 +0,0 @@
-'''01. Considere que as variáveis “dia”, “mês” e “ano” contém os valores respectivos de uma certa data. Escreva um comando “print”
-que imprima essa data no formato usado, por exemplo, “15/4/2020” ou “2/12/2004”.'''
-print('convendo no formato x-xx-xxxx')
-dia = int(input('Digite o dia desejado (Ex.: 10): '))
-mes = int(input('Digite o mes desejado (Ex.: 12): '))
-ano = int(input('Digite o ano desejado (Ex.: 2025): '))
-
-print(f'A data solicitada foi {dia}/{mes}/{ano}')
